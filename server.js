89bf5715038fbe9cf5883014802a7f4a2c64c84b
piotr kotulski <piotr.kotulski@exorigo-upos.pl>
2024-01-21 20:59:31 +0100

    init commit with gitignore

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const hbs = require('express-handlebars');
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', { layout: 'dark' });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+    res.render('info');
+});
+
+app.get('/history', (req, res, next) => {
+    res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
